6556b3516df8d726e8b2d8ca4ddeb96b9aa9b42b
francescasuba <subaap@mail.uc.edu>
2023-02-23 21:19:06 -0500

    Added inline comments, updated pub_date type, and initialized mvm

--- app/src/main/java/com/newsnow/dao/IArticleDAO.kt
+++ app/src/main/java/com/newsnow/dao/IArticleDAO.kt
@@ -5,7 +5,8 @@ import retrofit2.Call
 import retrofit2.http.GET
 
 interface IArticleDAO {
-
+    // API key is exclusive to Francesca
+    // TODO: How to have an encrypted api key?
     @GET("/api/1/news?apikey=pub_17584a2c69bac6e81deb6654731301f7095b1")
     fun getAllArticles(): Call<ArrayList<Article>>
 }
--- app/src/main/java/com/newsnow/dto/Article.kt
+++ app/src/main/java/com/newsnow/dto/Article.kt
@@ -10,9 +10,10 @@ data class Article(
     var link : String,
     var creator : String,
     var full_description : String,
-    var pubDate: Date,
+    var pubDate: String,
     @PrimaryKey var id : Int = 0
 ) {
+    // returns article title and full description
     override fun toString(): String {
         return title + " " + full_description
     }
