9f5c91e9ae5df16065dbf1d0bd508853cdccb927
spauldvl <spauldvl@mail.uc.edu>
2023-02-24 23:12:26 -0500

    UI and Theme additions

--- app/src/main/java/com/newsnow/MainActivity.kt
+++ app/src/main/java/com/newsnow/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.newsnow
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,6 +12,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import com.newsnow.ui.theme.NewsNowTheme
 
 
@@ -36,4 +38,16 @@ fun ArticleInfo(name : String) {
         onValueChange = {articleName = it},
         label = { Text(stringResource(R.string.articleName))}
         )
+}
+@Preview(name="Light Mode", showBackground=true)
+@Preview(uiMode= Configuration.UI_MODE_NIGHT_YES, showBackground = true, name="Dark Mode")
+@Composable
+fun DefaultPreview() {
+    NewsNowTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(color = MaterialTheme.colors.background,
+            modifier = Modifier.fillMaxWidth()) {
+            ArticleInfo("Android")
+        }
+    }
 }
