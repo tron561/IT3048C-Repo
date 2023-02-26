d1fe63e5c8957e527aa5793b84476af6f637a2a2
spauldvl <spauldvl@mail.uc.edu>
2023-02-26 14:51:39 -0500

    UI and Theme additions Created sample UI with dummy data

--- app/src/main/java/com/newsnow/MainActivity.kt
+++ app/src/main/java/com/newsnow/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
@@ -12,7 +13,10 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.newsnow.ui.theme.Grey
 import com.newsnow.ui.theme.NewsNowTheme
 
 
@@ -33,11 +37,52 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun ArticleInfo(name : String) {
     var articleName by remember { mutableStateOf("")}
-    OutlinedTextField(
-        value = articleName,
-        onValueChange = {articleName = it},
-        label = { Text(stringResource(R.string.articleName))}
+    // sample text until we learn to put JSON data into the UI
+    Column {
+        Text(
+            //where its found
+            text = "from the NYTimes...",
+            fontSize = 15.sp,
         )
+        Text(
+            // title
+            text = "Rihanna is BACK",
+            fontSize = 50.sp,
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center,
+        )
+        Text(
+            // author and date
+            text = "Jane Doe | 02/23/2023",
+            fontSize = 17.sp,
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center,
+        )
+        Text(
+            // sub title
+            text = "International superstars performs at the Super Bowl Halftime Show.",
+            fontSize = 23.sp,
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center,
+        )
+        Text(
+            //body text
+            text = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula" +
+                    " eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient" +
+                    " montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, " +
+                    "pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, " +
+                    "aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis " +
+                    "vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus." +
+                    " Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor " +
+                    "eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, " +
+                    "tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet." +
+                    " Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. ",
+            fontSize = 20.sp,
+            modifier = Modifier.fillMaxWidth(),
+
+
+        )
+    }
 }
 @Preview(name="Light Mode", showBackground=true)
 @Preview(uiMode= Configuration.UI_MODE_NIGHT_YES, showBackground = true, name="Dark Mode")
