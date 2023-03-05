a2e617b350501b14085ade3e6febba7a60b32783
Levi H <levihuff1207@gmail.com>
2023-03-05 16:53:41 -0500

    removed lorem ispum placeholder, passing article dto to articleinfo function, which displays text composible

--- app/src/main/java/com/newsnow/MainActivity.kt
+++ app/src/main/java/com/newsnow/MainActivity.kt
@@ -7,18 +7,17 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import com.newsnow.dto.Article
 import com.newsnow.ui.theme.Grey
 import com.newsnow.ui.theme.NewsNowTheme
-
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,17 +26,25 @@ class MainActivity : ComponentActivity() {
             NewsNowTheme {
                 // surface container background color
                 Surface(color = MaterialTheme.colors.background,
-                modifier = Modifier.fillMaxWidth()){
-                    ArticleInfo("Android")
+                    modifier = Modifier.fillMaxWidth()){
+                    ArticleInfo(
+                        Article(
+                            title = "Rihanna is BACK",
+                            link = "https://example.com/rihanna",
+                            creator = "Jane Doe",
+                            full_description = "International superstar performs at the Super Bowl Halftime Show.",
+                            pubDate = "2023-02-23",
+                            id = 1
+                        )
+                    )
                 }
             }
         }
     }
 }
+
 @Composable
-fun ArticleInfo(name : String) {
-    var articleName by remember { mutableStateOf("")}
-    // sample text until we learn to put JSON data into the UI
+fun ArticleInfo(article: Article) {
     Column {
         Text(
             //where its found
@@ -46,44 +53,28 @@ fun ArticleInfo(name : String) {
         )
         Text(
             // title
-            text = "Rihanna is BACK",
+            text = article.title,
             fontSize = 50.sp,
             modifier = Modifier.fillMaxWidth(),
             textAlign = TextAlign.Center,
         )
         Text(
             // author and date
-            text = "Jane Doe | 02/23/2023",
+            text = "${article.creator} | ${article.pubDate}",
             fontSize = 17.sp,
             modifier = Modifier.fillMaxWidth(),
             textAlign = TextAlign.Center,
         )
         Text(
             // sub title
-            text = "International superstars performs at the Super Bowl Halftime Show.",
+            text = article.full_description,
             fontSize = 23.sp,
             modifier = Modifier.fillMaxWidth(),
             textAlign = TextAlign.Center,
         )
-        Text(
-            //body text
-            text = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula" +
-                    " eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient" +
-                    " montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, " +
-                    "pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, " +
-                    "aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis " +
-                    "vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus." +
-                    " Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor " +
-                    "eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, " +
-                    "tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet." +
-                    " Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. ",
-            fontSize = 20.sp,
-            modifier = Modifier.fillMaxWidth(),
-
-
-        )
     }
 }
+
 @Preview(name="Light Mode", showBackground=true)
 @Preview(uiMode= Configuration.UI_MODE_NIGHT_YES, showBackground = true, name="Dark Mode")
 @Composable
@@ -92,7 +83,16 @@ fun DefaultPreview() {
         // A surface container using the 'background' color from the theme
         Surface(color = MaterialTheme.colors.background,
             modifier = Modifier.fillMaxWidth()) {
-            ArticleInfo("Android")
+            ArticleInfo(
+                Article(
+                    title = "Rihanna is BACK",
+                    link = "https://example.com/rihanna",
+                    creator = "Jane Doe",
+                    full_description = "International superstar performs at the Super Bowl Halftime Show.",
+                    pubDate = "2023-02-23",
+                    id = 1
+                )
+            )
         }
     }
 }
