f1276fe42cd4fa09a93f0f34b5cf4c4f5c7d94fd
daltonco <daltonco@mail.uc.edu>
2023-03-05 18:32:41 -0500

    Update RetrofitClientInstance.kt BASE_URL
    I changed the type of BASE_URL to include the const keyword. While not necessary to compile, const is used to declare a read-only property of a class. Since we are using retrofit to import mock JSON data right now, we do not need this to be a read/write variable. Const ensures that we cannot accidentally modify the JSON data.

--- app/src/main/java/com/newsnow/RetrofitClientInstance.kt
+++ app/src/main/java/com/newsnow/RetrofitClientInstance.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClientInstance {
     private var retrofit: Retrofit? = null
-    private val BASE_URL = "https://newsdata.io/"
+    private const val BASE_URL = "https://newsdata.io/"
 
     val retrofitInstance : Retrofit?
         get() {
