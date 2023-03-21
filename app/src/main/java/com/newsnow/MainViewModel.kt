18b989af0963dc10896108f8dad2cd31698dafbf
francescasuba <subaap@mail.uc.edu>
2023-03-21 15:02:43 -0400

    Add firestore to mvm

--- app/src/main/java/com/newsnow/MainViewModel.kt
+++ app/src/main/java/com/newsnow/MainViewModel.kt
@@ -3,13 +3,17 @@ package com.newsnow
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.google.firebase.firestore.FirebaseFirestore
 import com.newsnow.service.ArticleService
 import com.newsnow.dto.Article
 import kotlinx.coroutines.launch
 
+
 class MainViewModel : ViewModel()  {
     var articles : MutableLiveData<List<Article>> = MutableLiveData<List<Article>>()
     var articleService : ArticleService = ArticleService()
+
+    private lateinit var firestore : FirebaseFirestore
 
     fun fetchArticles() {
         viewModelScope.launch {
