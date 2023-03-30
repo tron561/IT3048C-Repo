22acdaddb75ba24ac2cc3f98b86f3c35fac32ef2
Levi H <levihuff1207@gmail.com>
2023-03-30 06:24:50 -0400

    added error handling for when koin does not start

--- app/src/main/java/com/newsnow/NewsNowApplication.kt
+++ app/src/main/java/com/newsnow/NewsNowApplication.kt
@@ -1,22 +1,32 @@
 package com.newsnow
 
 import android.app.Application
+import android.widget.Toast
 import com.newsnow.appModule
 import org.koin.android.BuildConfig
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.GlobalContext
+import org.koin.core.context.startKoin
 import org.koin.core.logger.Level
 
 class NewsNowApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        // Start Koin
-        GlobalContext.startKoin {
-            androidLogger(if (BuildConfig.DEBUG) Level.ERROR else Level.NONE)
-            androidContext(this@NewsNowApplication)
-            modules(appModule)
+        // Error handling for when Koin does not start
+        try {
+            startKoin {
+                androidLogger()
+                androidContext(this@NewsNowApplication)
+                modules(appModule)
+            }
+        } catch (e: IllegalStateException) {
+            Toast.makeText(
+                this@NewsNowApplication,
+                "Error starting Koin: ${e.message}",
+                Toast.LENGTH_LONG
+            ).show()
         }
     }
 
