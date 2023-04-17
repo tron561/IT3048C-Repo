0ca7cb7558e4c2c9fe84de89eefe77f568c47c49
francescasuba <subaap@mail.uc.edu>
2023-04-17 19:05:36 -0400

    made api key secret

--- app/src/main/java/com/newsnow/CurrentsAPIReq.kt
+++ app/src/main/java/com/newsnow/CurrentsAPIReq.kt
@@ -3,9 +3,11 @@ package com.newsnow
 import com.newsnow.api.CurrentsAPIJSON
 import retrofit2.http.GET
 
+const val apiKey: String = BuildConfig.API_KEY
+
 interface CurrentsAPIReq {
 
     //API Key is exclusive to account hedgesaj@mail.uc.edu
-        @GET("/v1/latest-news?language=en&apiKey=K4R1MTDCh9MMUtEGWVHJWl2b1YVQXnw_vk2RvxR4_m0rd1e8")
+        @GET("/v1/latest-news?language=en&apiKey=$apiKey")
         suspend fun getNews() : CurrentsAPIJSON
 }
